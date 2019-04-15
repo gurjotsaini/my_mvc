6cf9cd843d9f1bc71759bdc62ecb0c82aca7687b
gurjotsaini <gurjotsaini1992@gmail.com>
2019-04-15 21:10:53 +0530

    Update: controllers are now loading from the URL

---
+++ app/controllers/Pages.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * Created by User: gurjot
+     */
+
+    class Pages
+    {
+        public function __construct () {
+            echo "Pages loaded";
+        }
+    }
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -15,13 +15,42 @@
         protected $params               = [];
 
         public function __construct () {
-            $this->getUrl();
+//            print_r($this->getUrl());
+            $url = $this->getUrl();
+
+            // Look in controllers for first value
+            // Used ucwords() function to convert !st letter of URL value to uppercase
+            if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+                // If exists, set as controller
+                $this->currentController = ucwords($url[0]);
+
+                // Unset zero index
+                unset($url[0]);
+            } // End of if
+
+            // Require the controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            // Instantiate controller class
+            $this->currentController = new $this->currentController;
         }
 
         /**
          * Get URL value
          */
         public function getUrl() {
-            echo $_GET['url'];
+            // Check: URL is set or not
+            if (isset($_GET['url'])) {
+                // Strip the forward slash on end of URL
+                $url = rtrim($_GET['url'], '/');
+
+                // Sanitize the URL
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+
+                // Breaking values in an array
+                $url = explode('/', $url);
+
+                return $url;
+            }
         }
     }
